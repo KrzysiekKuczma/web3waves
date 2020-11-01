9d73cb7810a34e79909b58c9bb4d0e53a71960c4
Krzysiek Kuczma <kuczma.krzysiek@gmail.com>
2020-11-01 23:45:32 +0100

    created simple react app with waves keeper

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [authObject, setAuthObject] = useState<any>({});
+
+  const authData = { data: "This is vawes demo website" }
+  const keeperAuth = () => {
+    if (WavesKeeper) {
+      WavesKeeper.auth(authData)
+        .then(auth => {
+          setAuthObject(auth)
+          console.log(authObject)
+        })
+        .catch(err => console.log(err))
+    } else {
+      alert("Please install WavesKeeper to operate on this website");
+    }
+  }
+  const displayAuthData = () => {
+    if (authObject) {
+      return Object.keys(authObject).map(key => {
+        return `${key}: ${authObject[key]}`
+      }).join('\n');
+    }
+    return '';
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">This is a waves initial application</h1>
+        <button className="App-button" onClick={keeperAuth}>Click here to use WavesKeeper</button>
+        <div className="App-authData">
+          {displayAuthData()}
+        </div>
       </header>
     </div>
   );
